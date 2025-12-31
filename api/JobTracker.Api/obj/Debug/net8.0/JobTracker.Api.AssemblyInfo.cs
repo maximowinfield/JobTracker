ac79bbcff87eb9852743a903fe39ae4143c40338
Maximo Winfield <maximo.winfield.7@gmail.com>
2025-12-31 09:56:07 -0500

    misc

--- api/JobTracker.Api/obj/Debug/net8.0/JobTracker.Api.AssemblyInfo.cs
+++ api/JobTracker.Api/obj/Debug/net8.0/JobTracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61722be8fea76aa8829ed37f1cfc3ac4f0e1462e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f89fe92e299ff5e2f4da6dd1306bf10403acf66")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
